6de3c0d2fa911d4afa629c979b1fe444a46b6803
fransiska <fransiska.basoeki@gmail.com>
2024-01-31 08:38:15 +0900

    fixup! Write file in python

--- script/convert.py
+++ script/convert.py
@@ -9,8 +9,8 @@ def main():
     a = np.arange(15).reshape(3, 5)
     print("array", a)
     with open("converted.txt", "w") as f:
-        f.writeline(datetime.now())
-        f.writeline(os.environ.get("CHANGED_FILES"))
+        f.write(datetime.now().isoformat())
+        f.write(os.environ.get("CHANGED_FILES"))
 
 if __name__ == "__main__":
     main()
