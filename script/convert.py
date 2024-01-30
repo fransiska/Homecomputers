d61c25fc2cdcad51308826b89321e941043df187
fransiska <fransiska.basoeki@gmail.com>
2024-01-31 08:27:28 +0900

    Get changed files

--- script/convert.py
+++ script/convert.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+
+import os
 import numpy as np
 
 def main():
+    print("Changed files", os.environ["CHANGED_FILES"])
     a = np.arange(15).reshape(3, 5)
     print("array", a)
 
